5adb7ff2a7af7c94537a5340015c70edce914aa5
veronica365 <naruth10@gmail.com>
2023-02-25 07:15:46 +0300

    Add functionality to ensure that the brackets are balanced

--- challenge.js
+++ challenge.js
@@ -1,14 +1,36 @@
-function balancedBrackets(string) {
-  // your code here
+const matches = { "[": "]", "(": ")", "{": "}" };
+// Function to check if brackets are balanced
+function balancedBrackets(expr) {
+  let stack = [];
+
+  for (let i = 0; i < expr.length; i++) {
+    let x = expr[i];
+
+    if (matches[x]) {
+      stack.push(x);
+      continue;
+    }
+
+    // If current character is not opening
+    // bracket, then it must be closing.
+    // So stack cannot be empty at this point.
+    if (stack.length === 0) return false;
+    if (![")", "}", "]"].includes(x)) continue;
+    const balance = { ")": "{[", "}": "([", "]": "{(" };
+    let check = stack.pop();
+    if (balance[x].includes(check)) return false;
+  }
+
+  return stack.length === 0;
 }
 
-console.log(balancedBrackets('(hello)[world]'))
+console.log(balancedBrackets("(hello)[world]"));
 // => true
 
-console.log(balancedBrackets('([)]'))
+console.log(balancedBrackets("([)]"));
 // => false
 
-console.log(balancedBrackets('[({}{}{})([])]'))
+console.log(balancedBrackets("[({}{}{})([])]"));
 // => true
 
-module.exports = balancedBrackets
+module.exports = balancedBrackets;
